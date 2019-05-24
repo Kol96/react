b1f2329cdeb543227aac552f9c587910e5b343bc
陈超伦 <chenchaolun@hyperchain.cn>
2019-05-24 19:38:30 +0800

    ReactCreateRef

--- packages/react/src/ReactCreateRef.js
+++ packages/react/src/ReactCreateRef.js
@@ -10,10 +10,13 @@ import type {RefObject} from 'shared/ReactTypes';
 
 // an immutable object with a single mutable value
 export function createRef(): RefObject {
+  // HTML元素 current会接收DOM元素
+  // class组件 current会接收组件实例
   const refObject = {
     current: null,
   };
   if (__DEV__) {
+    // 不能添加新的值也不能删除 但可以修改现有属性值 并将所有现有属性标记为不可配置
     Object.seal(refObject);
   }
   return refObject;
